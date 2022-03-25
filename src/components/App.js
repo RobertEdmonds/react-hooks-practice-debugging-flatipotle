420f0af8e3be7c2053aeb25dbac4c5dfcc78e232
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-03-25 17:22:18 -0600

    commit tree 746aca716b390a249305d228803bdf25cc4124b7

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,22 @@ import "./App.css";
 function App() {
   const [orders, setOrders] = useState([]);
 
-  function addOrder(order) {
-    setOrders(orders.concat(order));
+    const addOrder = (order) => {
+    setOrders([...orders, order]);
   }
 
-  const displayOrders = orders.map((order, idx) => {
-    <Order key={idx} {...order} />;
-  });
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Form />
+      <Form addOrder={addOrder}/>
       <div className="ui raised container segment">
         <h1 className="ui block header">All Orders</h1>
-        <div className="ui three cards">{displayOrders}</div>
+        <div className="ui three cards">{orders.map((order, idx) => {
+    return <Order key={idx} order={order} />;
+  })}</div>
       </div>
     </div>
   );
